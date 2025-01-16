76205624d71038fd2be88dbc3e2bbf9adb52f9e3
AndriusCarmo <amjc2@poli.br>
2025-01-16 06:03:53 -0300

    Criando o servidor

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ import http from "http";
 const server= http.createServer((req,res)=>{
     res.writeHead(200,{ "Content-type": "text/plain"});
     res.end("Curso de Node.js");
-
 });
+
+server.listen(3000,()=>{
+    console.log("Servidor escutando !");
+})
