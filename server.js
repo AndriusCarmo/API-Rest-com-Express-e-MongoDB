9ae7f79ba424c6d17c340e8b570c50e11e1878a6
AndriusCarmo <amjc2@poli.br>
2025-01-17 21:14:56 -0300

    Criando rotas

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import http from "http";
+const PORT = 3000;
+
+const rotas ={
+    "/": "Curso de Node.js",
+    "/livros":"Entrei na rota livros",
+    "/autores":"Entrei na rota autores"
+}
 
 const server= http.createServer((req,res)=>{
     res.writeHead(200,{ "Content-type": "text/plain"});
-    res.end("Curso de Node.js");
+    
+    res.end(rotas[req.url]);
+    
 });
 
-server.listen(3000,()=>{
+server.listen(PORT,()=>{
     console.log("Servidor escutando !");
 })
