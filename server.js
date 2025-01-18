82d41c0c04e28d45ac64426076a1ac9bbafac259
AndriusCarmo <amjc2@poli.br>
2025-01-17 21:41:17 -0300

    instalando o nodemon

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const PORT = 3000;
 
 const rotas ={
     "/": "Curso de Node.js",
-    "/livros":"Entrei na rota livros",
-    "/autores":"Entrei na rota autores"
+    "/livros":"rota livros",
+    "/autores":"rota autores"
 }
 
 const server= http.createServer((req,res)=>{
     res.writeHead(200,{ "Content-type": "text/plain"});
     
     res.end(rotas[req.url]);
-    
+
 });
 
 server.listen(PORT,()=>{
