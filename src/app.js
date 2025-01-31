aa6dd2a5b0711f9ce96c7a1353c140e4041f9076
AndriusCarmo <amjc2@poli.br>
2025-01-31 08:32:33 -0300

    refatorando listarLivro e listarLivroPorId

--- src/app.js
+++ src/app.js
@@ -16,15 +16,6 @@ const app = express();
 routes(app);
 
 
-app.get("/livros/:id",(req,res)=>{
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-})
-
-app.post("/livros",(req,res)=>{
-    livros.push(req.body);
-    res.status(201).send("Livro cadastrado");
-},)
 
 app.put("/livros/:id",(req,res)=>{
     const index = buscaLivro(req.params.id);
