ffd15d03270178850ce950f03a17de261fe02883
AndriusCarmo <amjc2@poli.br>
2025-01-31 09:09:18 -0300

    Controller DELETE

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -27,7 +27,7 @@ class LivroController {
         }catch(erro){
             res.status(500).json({message: `${erro.message} - falha ao cadastrar livro`})
         }
-    }
+    };
 
     static async atualizarLivro(req,res){
         try{
@@ -36,6 +36,16 @@ class LivroController {
             res.status(200).json({message:"Livro atualizado"});
         }catch(erro){
             res.status(500).json({messagea:`${erro.message} - Falha na atualizaçõa do livro`});
+        }
+    };
+
+    static async excluirLivroPorId(req,res){
+        try{
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({message:"Livro removido com sucesso"});
+        }catch(erro){
+            res.status(500).json({message:`${erro.message} - falha ao deletar livro`});
         }
     };
 
